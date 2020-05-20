40eb10da4311489edb3a3e9c4f60c8b5ebfbe411
Scott Madeira <scott@mygcc.org>
2020-05-20 15:27:18 -0400

    added custom CSS for group details

--- resources/views/widgets/groupdetails.blade.php
+++ resources/views/widgets/groupdetails.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container-fluid col-xs-12 col-md-10 col-md-offset-1 ">
 
-        <mpp-group-details returnurl="/groupfinder"></mpp-group-details>
+        <mpp-group-details returnurl="/groupfinder" customcss="https://connect.mygcc.org/Content/grace-widgetstyles.css"></mpp-group-details>
 
     </div>
 @endsection
