b36011fea8f65a5eabe77eb171b679d365898f54
Scott Madeira <scott@mygcc.org>
2020-05-22 08:09:16 -0400

    Added details page with signup

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/groupfinder', function(){ return view('widgets.groups'); });
 Route::get('/groupdetails', function(){ return view('widgets.groupdetails'); });
+Route::get('/groupdetails2', function(){ return view('widgets.groupdetails-with-signup'); });
 Route::get('/eventfinder', function(){ return view('widgets.events'); });
 Route::get('/visit', function(){ return view('widgets.planvisit'); });
 Route::get('/myhousehold', function(){ return view('widgets.myhousehold'); });
